a12cba974fb51413ed9bc0c90bc4796ca102cc96
kang77649119 <77649119@qq.com>
2016-12-25 15:09:07 +0800

    修改： 	立即显示切换效果

--- DynamicNameSpaceDemo/DynamicNameSpaceDemo/Classes/Home/HomeVC.swift
+++ DynamicNameSpaceDemo/DynamicNameSpaceDemo/Classes/Home/HomeVC.swift
@@ -18,7 +18,7 @@ class HomeVC: UIViewController {
         let btnW = UIScreen.main.bounds.width - 20
         let btn = UIButton(frame: CGRect(x: 10, y: 100, width: btnW, height: 30))
         btn.backgroundColor = UIColor.gray
-        btn.setTitle("点击我,然后重启app,观察切换底部tabBar", for: .normal)
+        btn.setTitle("点击我,观察切换底部tabBar", for: .normal)
         btn.addTarget(self, action: #selector(self.changeTabBar), for: .touchUpInside)
         self.view.addSubview(btn)
         
@@ -29,6 +29,8 @@ class HomeVC: UIViewController {
         
         let isChange = UserDefaults.standard.bool(forKey: "changeTabBar")
         UserDefaults.standard.set(!isChange, forKey: "changeTabBar")
+        
+        UIApplication.shared.windows.last?.rootViewController = MainTabBarController()
         
     }
 
